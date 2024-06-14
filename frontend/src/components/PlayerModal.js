9491224d9aef3e7add5d630dcb4c236c4770ef25
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-06-13 22:56:02 -0400

    more stats

--- frontend/src/components/PlayerModal.js
+++ frontend/src/components/PlayerModal.js
@@ -1,24 +1,108 @@
-import React from "react";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-  Typography,
-} from "@mui/material";
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 
-function PlayerModal({ player, onClose }) {
+function PlayerModal({ player, playerStats, onClose }) {
+  const { seasonTotalsRegularSeason, seasonTotalsPostSeason, careerHighs } = playerStats;
+
+  const renderTable = (stats) => (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Season</TableCell>
+          <TableCell>Team</TableCell>
+          <TableCell>GP</TableCell>
+          <TableCell>GS</TableCell>
+          <TableCell>MIN</TableCell>
+          <TableCell>FGM</TableCell>
+          <TableCell>FGA</TableCell>
+          <TableCell>FG%</TableCell>
+          <TableCell>3PM</TableCell>
+          <TableCell>3PA</TableCell>
+          <TableCell>3P%</TableCell>
+          <TableCell>FTM</TableCell>
+          <TableCell>FTA</TableCell>
+          <TableCell>FT%</TableCell>
+          <TableCell>OREB</TableCell>
+          <TableCell>DREB</TableCell>
+          <TableCell>REB</TableCell>
+          <TableCell>AST</TableCell>
+          <TableCell>STL</TableCell>
+          <TableCell>BLK</TableCell>
+          <TableCell>TOV</TableCell>
+          <TableCell>PF</TableCell>
+          <TableCell>PTS</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {stats.map((season) => (
+          <TableRow key={season.seasonId}>
+            <TableCell>{season.seasonId}</TableCell>
+            <TableCell>{season.teamAbbreviation}</TableCell>
+            <TableCell>{season.gp}</TableCell>
+            <TableCell>{season.gs}</TableCell>
+            <TableCell>{season.min}</TableCell>
+            <TableCell>{season.fgm}</TableCell>
+            <TableCell>{season.fga}</TableCell>
+            <TableCell>{season.fgPct}</TableCell>
+            <TableCell>{season.fG3M}</TableCell>
+            <TableCell>{season.fG3A}</TableCell>
+            <TableCell>{season.fg3Pct}</TableCell>
+            <TableCell>{season.ftm}</TableCell>
+            <TableCell>{season.fta}</TableCell>
+            <TableCell>{season.ftPct}</TableCell>
+            <TableCell>{season.oreb}</TableCell>
+            <TableCell>{season.dreb}</TableCell>
+            <TableCell>{season.reb}</TableCell>
+            <TableCell>{season.ast}</TableCell>
+            <TableCell>{season.stl}</TableCell>
+            <TableCell>{season.blk}</TableCell>
+            <TableCell>{season.tov}</TableCell>
+            <TableCell>{season.pf}</TableCell>
+            <TableCell>{season.pts}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+
   return (
     <Dialog open={Boolean(player)} onClose={onClose}>
       <DialogTitle>{player.name}</DialogTitle>
       <DialogContent>
         <Typography>Team: {player.team}</Typography>
         <Typography>Position: {player.position}</Typography>
-        <Typography>Points: {player.points}</Typography>
-        <Typography>Assists: {player.assists}</Typography>
-        <Typography>Rebounds: {player.rebounds}</Typography>
-        {/* Add more player info as needed */}
+
+        <Typography variant="h6">Regular Season Stats</Typography>
+        {renderTable(seasonTotalsRegularSeason)}
+
+        {seasonTotalsPostSeason.length > 0 && (
+          <>
+            <Typography variant="h6">Post Season Stats</Typography>
+            {renderTable(seasonTotalsPostSeason)}
+          </>
+        )}
+
+        <Typography variant="h6">Career Highs</Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Stat</TableCell>
+              <TableCell>Value</TableCell>
+              <TableCell>Game Date</TableCell>
+              <TableCell>Opponent</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {careerHighs.map((high) => (
+              <TableRow key={high.stat}>
+                <TableCell>{high.stat}</TableCell>
+                <TableCell>{high.statValue}</TableCell>
+                <TableCell>{high.gameDate}</TableCell>
+                <TableCell>{high.vsTeamName}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>Close</Button>

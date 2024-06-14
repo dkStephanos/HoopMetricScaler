1b1f5e0f19e5218305e68dcf81733958d57b13ec
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-06-14 00:01:50 -0400

    fixing spacing

--- frontend/src/components/PlayerModal.js
+++ frontend/src/components/PlayerModal.js
@@ -7,42 +7,34 @@ import {
   Button,
   Typography,
   CircularProgress,
-  Box,
 } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
 function PlayerModal({ player, playerStats, onClose }) {
   const columns = [
-    { field: "seasonId", headerName: "Season", width: 100 },
-    { field: "teamAbbreviation", headerName: "Team", width: 100 },
-    { field: "gp", headerName: "GP", width: 50 },
-    { field: "gs", headerName: "GS", width: 50 },
-    { field: "min", headerName: "MIN", width: 50 },
-    { field: "fgm", headerName: "FGM", width: 50 },
-    { field: "fga", headerName: "FGA", width: 50 },
-    { field: "fgPct", headerName: "FG%", width: 50 },
-    { field: "fG3M", headerName: "3PM", width: 50 },
-    { field: "fG3A", headerName: "3PA", width: 50 },
-    { field: "fg3Pct", headerName: "3P%", width: 50 },
-    { field: "ftm", headerName: "FTM", width: 50 },
-    { field: "fta", headerName: "FTA", width: 50 },
-    { field: "ftPct", headerName: "FT%", width: 50 },
-    { field: "oreb", headerName: "OREB", width: 50 },
-    { field: "dreb", headerName: "DREB", width: 50 },
-    { field: "reb", headerName: "REB", width: 50 },
-    { field: "ast", headerName: "AST", width: 50 },
-    { field: "stl", headerName: "STL", width: 50 },
-    { field: "blk", headerName: "BLK", width: 50 },
-    { field: "tov", headerName: "TOV", width: 50 },
-    { field: "pf", headerName: "PF", width: 50 },
-    { field: "pts", headerName: "PTS", width: 50 },
-  ];
-
-  const careerHighColumns = [
-    { field: "stat", headerName: "Stat", width: 100 },
-    { field: "statValue", headerName: "Value", width: 100 },
-    { field: "gameDate", headerName: "Game Date", width: 150 },
-    { field: "vsTeamName", headerName: "Opponent", width: 150 },
+    { field: "seasonId", headerName: "Season", flex: 1, minWidth: 100 },
+    { field: "teamAbbreviation", headerName: "Team", flex: 1, minWidth: 100 },
+    { field: "gp", headerName: "GP", flex: 1, minWidth: 50 },
+    { field: "gs", headerName: "GS", flex: 1, minWidth: 50 },
+    { field: "min", headerName: "MIN", flex: 1, minWidth: 50 },
+    { field: "fgm", headerName: "FGM", flex: 1, minWidth: 50 },
+    { field: "fga", headerName: "FGA", flex: 1, minWidth: 50 },
+    { field: "fgPct", headerName: "FG%", flex: 1, minWidth: 60 },
+    { field: "fG3M", headerName: "3PM", flex: 1, minWidth: 50 },
+    { field: "fG3A", headerName: "3PA", flex: 1, minWidth: 50 },
+    { field: "fg3Pct", headerName: "3P%", flex: 1, minWidth: 60 },
+    { field: "ftm", headerName: "FTM", flex: 1, minWidth: 50 },
+    { field: "fta", headerName: "FTA", flex: 1, minWidth: 50 },
+    { field: "ftPct", headerName: "FT%", flex: 1, minWidth: 60 },
+    { field: "oreb", headerName: "OREB", flex: 1, minWidth: 60 },
+    { field: "dreb", headerName: "DREB", flex: 1, minWidth: 50 },
+    { field: "reb", headerName: "REB", flex: 1, minWidth: 50 },
+    { field: "ast", headerName: "AST", flex: 1, minWidth: 50 },
+    { field: "stl", headerName: "STL", flex: 1, minWidth: 50 },
+    { field: "blk", headerName: "BLK", flex: 1, minWidth: 50 },
+    { field: "tov", headerName: "TOV", flex: 1, minWidth: 50 },
+    { field: "pf", headerName: "PF", flex: 1, minWidth: 50 },
+    { field: "pts", headerName: "PTS", flex: 1, minWidth: 50 },
   ];
 
   const addIdToRows = (rows) =>
@@ -62,36 +54,38 @@ function PlayerModal({ player, playerStats, onClose }) {
               <DataGrid
                 rows={addIdToRows(playerStats.seasonTotalsRegularSeason)}
                 columns={columns}
-                hideFooterPagination
-                hideFooterSelectedRowCount
+                autoHeight
+                hideFooter
               />
             </div>
 
             {playerStats.seasonTotalsPostSeason?.length > 0 && (
               <>
-                <Typography variant="h6" style={{ marginTop: 20 }}>
+                <Typography variant="h6" style={{ marginTop: 60 }}>
                   Post Season Stats
                 </Typography>
                 <div style={{ height: 400, width: "100%" }}>
                   <DataGrid
                     rows={addIdToRows(playerStats.seasonTotalsPostSeason)}
                     columns={columns}
-                    hideFooterPagination
-                    hideFooterSelectedRowCount
+                    autoHeight
+                    hideFooter
                   />
                 </div>
               </>
             )}
           </>
         ) : (
-          <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            height="100%"
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: 400,
+            }}
           >
             <CircularProgress />
-          </Box>
+          </div>
         )}
       </DialogContent>
       <DialogActions>

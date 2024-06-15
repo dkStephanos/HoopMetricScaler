e4e6755fa18350d5065b04bad20c7936be2c2e1c
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-06-14 23:56:29 -0400

    enhancing logging

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { getExtendedPlayerInfo } = require('./utils');
 const nba = require('nba');
+const logger = require('./logger'); // Import the logger
 const app = express();
 const port = 3001;
 
@@ -11,10 +12,12 @@ app.get('/player-stats/:id', async (req, res) => {
     const { id } = req.params;
     const { season } = req.query;
     try {
+        logger.info(`Fetching player stats for PlayerID: ${id}, Season: ${season}`);
         const playerStats = await nba.stats.playerProfile({ PlayerID: id, Season: season });
         const extendedPlayerInfo = await getExtendedPlayerInfo(id);
         res.json({ ...playerStats, ...extendedPlayerInfo });
     } catch (error) {
+        logger.error(`Error fetching player stats: ${error.message}`);
         res.status(500).send('Error fetching player stats');
     }
 });
@@ -23,6 +26,7 @@ app.get('/team-player-dashboard/:teamId', async (req, res) => {
     const { teamId } = req.params;
     const { season } = req.query;
     try {
+        logger.info(`Fetching team player dashboard for TeamID: ${teamId}, Season: ${season}`);
         const teamPlayerDashboard = await nba.stats.teamPlayerDashboard({ TeamID: teamId, Season: season });
         const players = teamPlayerDashboard.playersSeasonTotals;
         res.json({
@@ -30,11 +34,11 @@ app.get('/team-player-dashboard/:teamId', async (req, res) => {
             playersSeasonTotals: players
         });
     } catch (error) {
-        console.error('Error fetching team player dashboard:', error);
+        logger.error(`Error fetching team player dashboard: ${error.message}`);
         res.status(500).send('Error fetching team player dashboard');
     }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    logger.info(`Server is running on port ${port}`);
 });

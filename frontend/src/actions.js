120c2eddcb958a17c566f7d37a45da85ee15dcdf
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-06-15 23:15:53 -0400

    Update actions.js

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -25,3 +25,29 @@ export const fetchPlayerStats = async (playerId, season) => {
     console.error("Error fetching player stats:", error);
   }
 };
+
+export const fetchScaledStats = async (minutes, usage, selectedRows, init) => {
+  try {
+    const response = await fetch("http://localhost:3001/scale-stats", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        minutes,
+        usage,
+        selectedRows,
+        init,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.scaledStats || data.normalizedRows[0]
+  } catch (error) {
+    console.error("Error fetching scaled stats:", error);
+  }
+};
